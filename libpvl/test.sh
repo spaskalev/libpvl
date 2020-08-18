363c816bf30403bcb59533446ceb32cf2e04c736
Stanislav Paskalev <spaskalev@protonmail.com>
2020-08-18 23:28:08 +0300

    gcov integration: compile and test shared first, static as second

--- libpvl/test.sh
+++ libpvl/test.sh
@@ -16,31 +16,34 @@ IMPL_GUARD="WARNING_DO_NOT_INCLUDE_PLV_C"
 
 GC_BASE_OPTS="-g -fstrict-aliasing -Wall -Wextra"
 
-GC_LIB_OPTS="${GC_BASE_OPTS} -D${IMPL_GUARD}"
-GC_TEST_OPTS="${GC_BASE_OPTS} -fprofile-arcs -ftest-coverage -lgcov --coverage"
+#  -fprofile-arcs -ftest-coverage
+# --coverage -lgcov
+
+GC_LIB_OPTS="${GC_BASE_OPTS} -D${IMPL_GUARD} --coverage"
+GC_TEST_OPTS="${GC_BASE_OPTS} --coverage"
 
 # Clear
 rm -f "${TEST_BIN} ${LIB_OBJECT} ${LIB_SHARED} ${LIB_STATIC}" *.gcda *.gcno
 
 # Compile and link the lib (shared)
 gcc ${GC_LIB_OPTS} -c -fpic ${LIB_SRC} -o ${LIB_OBJECT}
-gcc -shared ${LIB_OBJECT} -o ${LIB_SHARED}
-
-# Compile and archive the lib (static)
-gcc ${GC_LIB_OPTS} -c ${LIB_SRC} -o ${LIB_OBJECT}
-ar rcs ${LIB_STATIC} ${LIB_OBJECT}
+gcc -shared ${GC_LIB_OPTS} ${LIB_OBJECT} -o ${LIB_SHARED}
 
 # Compile and run the iface tests (shared)
 gcc ${GC_TEST_OPTS} -L. -lpvl ${TEST_SRC_IFACE} -o ${TEST_BIN}
 LD_LIBRARY_PATH=".:${LD_LIBRARY_PATH}" ./${TEST_BIN}
 
-# Compile and run the iface tests (static)
-gcc -static ${GC_TEST_OPTS} ${TEST_SRC_IFACE} ${LIB_STATIC} -o ${TEST_BIN}
-./${TEST_BIN}
-
 # Compile and run the impl tests (shared)
 gcc ${GC_TEST_OPTS} -L. -lpvl ${TEST_SRC_IMPL} -o ${TEST_BIN}
 LD_LIBRARY_PATH=".:${LD_LIBRARY_PATH}" ./${TEST_BIN}
+
+# Compile and archive the lib (static)
+gcc ${GC_LIB_OPTS} -c ${LIB_SRC} -o ${LIB_OBJECT}
+ar rcs ${LIB_STATIC} ${LIB_OBJECT}
+
+# Compile and run the iface tests (static)
+gcc -static ${GC_TEST_OPTS} ${TEST_SRC_IFACE} ${LIB_STATIC} -o ${TEST_BIN}
+./${TEST_BIN}
 
 # Compile and run the impl tests (static)
 gcc -static ${GC_TEST_OPTS} ${TEST_SRC_IMPL} ${LIB_STATIC} -o ${TEST_BIN}
