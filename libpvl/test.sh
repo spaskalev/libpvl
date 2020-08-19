6e02c653180c11dcc1226ad071186fcba2e96b49
Stanislav Paskalev <spaskalev@protonmail.com>
2020-08-19 04:38:36 +0300

    add -pendantic to CFLAGS

--- libpvl/test.sh
+++ libpvl/test.sh
@@ -17,7 +17,9 @@ BRANCH_COV="50"
 
 IMPL_GUARD="WARNING_DO_NOT_INCLUDE_PLV_C"
 
-GC_BASE_OPTS="-g -fstrict-aliasing -Wall -Wextra"
+CC="gcc"
+
+GC_BASE_OPTS="-g -fstrict-aliasing -pedantic -Wall -Wextra"
 GC_LIB_OPTS="${GC_BASE_OPTS} -D${IMPL_GUARD} --coverage"
 GC_TEST_OPTS="${GC_BASE_OPTS} --coverage"
 
@@ -32,22 +34,22 @@ function __clean() {
 }
 
 function __compile_shared_lib() {
-    gcc ${GC_OPT_LEVEL} ${GC_LIB_OPTS} -c -fpic ${LIB_SRC} -o ${LIB_OBJECT}
-    gcc -shared ${GC_OPT_LEVEL} ${GC_LIB_OPTS} ${LIB_OBJECT} -o ${LIB_SHARED}
+    ${CC} ${GC_OPT_LEVEL} ${GC_LIB_OPTS} -c -fpic ${LIB_SRC} -o ${LIB_OBJECT}
+    ${CC} -shared ${GC_OPT_LEVEL} ${GC_LIB_OPTS} ${LIB_OBJECT} -o ${LIB_SHARED}
 }
 
 function __compile_and_run_shared_test() {
-    gcc ${GC_OPT_LEVEL} ${GC_TEST_OPTS} -L. -lpvl ${1} -o ${TEST_BIN}
+    ${CC} ${GC_OPT_LEVEL} ${GC_TEST_OPTS} -L. -lpvl ${1} -o ${TEST_BIN}
     LD_LIBRARY_PATH=".:${LD_LIBRARY_PATH}" ./${TEST_BIN}
 }
 
 function __compile_static_lib() {
-    gcc ${GC_OPT_LEVEL} ${GC_LIB_OPTS} -c ${LIB_SRC} -o ${LIB_OBJECT}
+    ${CC} ${GC_OPT_LEVEL} ${GC_LIB_OPTS} -c ${LIB_SRC} -o ${LIB_OBJECT}
     ar rcs ${LIB_STATIC} ${LIB_OBJECT}
 }
 
 function __compile_and_run_static_test() {
-    gcc -static ${GC_OPT_LEVEL} ${GC_TEST_OPTS} ${1} ${LIB_STATIC} -o ${TEST_BIN}
+    ${CC} -static ${GC_OPT_LEVEL} ${GC_TEST_OPTS} ${1} ${LIB_STATIC} -o ${TEST_BIN}
     ./${TEST_BIN}
 }
 
@@ -55,7 +57,11 @@ function __coverage() {
 	if [ "${GC_OPT_LEVEL}" != "-O0" ]; then
 		echo "Skipping coverage for optimization level ${GC_OPT_LEVEL}"
 		return 0
-	fi	
+	fi
+	if [ "${CC}" != "gcc" ]; then
+		echo "Skipping coverage for compiler ${CC}"
+		return 0
+	fi
     gcovr --fail-under-line ${LINE_COV}
     gcovr --branches --fail-under-branch ${BRANCH_COV}
 }
