938ab57f7497e1d0f84822b56076b56716a9eb75
Stanislav Paskalev <spaskalev@protonmail.com>
2020-11-08 13:45:37 +0200

    add a basic mirror test in e2e, fix a memory leak in e2e tests, bump coverage

--- libpvl/tests_e2e.c
+++ libpvl/tests_e2e.c
@@ -54,6 +54,7 @@ typedef struct {
     pvl_t* pvl;
     alignas(max_align_t) char pvl_at[1024];
     alignas(max_align_t) char buf[1024];
+    alignas(max_align_t) char mirror[1024];
 
     char*  dyn_buf;
     size_t dyn_len;
@@ -110,6 +111,74 @@ int post_save(pvl_t* pvl, int full, size_t length, FILE* file, int failed) {
 }
 
 void test_basic() {
+    int written_length = 96;
+    int marks_count = 10;
+
+    ctx = (test_ctx){0};
+
+    ctx.pvl = pvl_init(ctx.pvl_at, marks_count, ctx.buf, 1024, ctx.mirror, NULL, NULL, pre_save, post_save, NULL);
+    assert(ctx.pvl != NULL);
+
+    ctx.dyn_file = open_memstream(&ctx.dyn_buf, &ctx.dyn_len);
+
+    ctx.pre_save.return_file = ctx.dyn_file;
+    ctx.pre_save.expected_pvl = ctx.pvl;
+    ctx.pre_save.expected_full = 0;
+    ctx.pre_save.expected_length = written_length;
+
+    ctx.post_save.return_int = 0;
+    ctx.post_save.expected_pvl = ctx.pvl;
+    ctx.post_save.expected_full = 0;
+    ctx.post_save.expected_length = written_length;
+    ctx.post_save.expected_file = ctx.dyn_file;
+    ctx.post_save.expected_failed = 0;
+
+    // Write and commit some data
+    assert(!pvl_begin(ctx.pvl));
+    memset(ctx.buf, 1, 64);
+    assert(!pvl_mark(ctx.pvl, ctx.buf, 64));
+    assert(!pvl_commit(ctx.pvl));
+
+    assert(ctx.pre_save.call_count == 1);
+    assert(ctx.post_save.call_count == 1);
+
+    // Prepare for reading the data
+    rewind(ctx.dyn_file);
+    memset(ctx.pvl_at, 0, 1024);
+    memset(ctx.buf, 0, 1024);
+
+    ctx.pre_load.return_file = ctx.dyn_file;
+    ctx.pre_load.expected_pvl = ctx.pvl;
+    ctx.pre_load.expected_initial = 1;
+    ctx.pre_load.expected_file = NULL;
+    ctx.pre_load.expected_up_to_pos = 0;
+
+    ctx.post_load.return_int = 0;
+    ctx.post_load.expected_pvl = ctx.pvl;
+    ctx.post_load.expected_file = ctx.dyn_file;
+    ctx.post_load.expected_failed = 0;
+    ctx.post_load.expected_last_good = written_length;
+
+    // Create a new pvl to load the data
+    ctx.pvl = pvl_init(ctx.pvl_at, marks_count, ctx.buf, 1024, NULL, pre_load, post_load, NULL, NULL, NULL);
+
+    assert(ctx.pre_load.call_count == 1);
+    assert(ctx.post_load.call_count == 1);
+
+    // Verify it
+    for (size_t i = 0; i < 1024; i++) {
+        if (i < 64) {
+            assert(ctx.buf[i] == 1);
+        } else {
+            assert(ctx.buf[i] == 0);
+        }
+    }
+
+    assert(!fclose(ctx.dyn_file));
+    free(ctx.dyn_buf);
+}
+
+void test_basic_mirror() {
     int written_length = 96;
     int marks_count = 10;
 
@@ -172,8 +241,12 @@ void test_basic() {
             assert(ctx.buf[i] == 0);
         }
     }
+
+    assert(!fclose(ctx.dyn_file));
+    free(ctx.dyn_buf);
 }
 
 int main() {
     test_basic();
+    test_basic_mirror();
 }
