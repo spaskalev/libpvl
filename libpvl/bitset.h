0ff680bb49b717ae674908526dca262ab96b0e86
Stanislav Paskalev <spaskalev@protonmail.com>
2021-01-19 23:43:58 +0200

    extract bitset to bitset.h

---
+++ libpvl/bitset.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Stanislav Paskalev <spaskalev@protonmail.com>
+ */
+
+/*
+ * A char-backed bitset implementation
+ */
+#pragma once
+
+size_t bitset_size(size_t elements) {
+	return ((elements) + CHAR_BIT - 1u) / CHAR_BIT;
+}
+
+void bitset_set(unsigned char *bitset, size_t from_pos, size_t to_pos) {
+	const unsigned char cbits[8] = {1, 3, 7, 15, 31, 63, 127, 255};
+    size_t from_bucket = from_pos / CHAR_BIT;
+    size_t from_index = from_pos % CHAR_BIT;
+    size_t to_bucket = to_pos / CHAR_BIT;
+    size_t to_index = to_pos % CHAR_BIT;
+    if (from_bucket == to_bucket) {
+		bitset[from_bucket] |= (size_t)(cbits[to_index - from_index]) << from_index;
+	} else {
+		bitset[from_bucket] |= (size_t)(cbits[7u - from_index]) << from_index;
+		for (size_t i = from_bucket+1; i < to_bucket; i++) {
+			bitset[i] = 255u;
+		}
+		bitset[to_bucket] |= cbits[to_index];
+	}
+}
+
+_Bool bitset_test(const unsigned char *bitset, size_t pos) {
+    size_t bucket = pos / CHAR_BIT;
+    size_t index = pos % CHAR_BIT;
+    return (_Bool)(bitset[bucket] & (1u << index));
+}
