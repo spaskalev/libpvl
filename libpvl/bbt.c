6d208fa7233bfde7dffafbcb654a7ff4f3f69b8b
Stanislav Paskalev <spaskalev@protonmail.com>
2021-01-24 00:04:57 +0200

    bbt: use size_t-based type for position tracking

--- libpvl/bbt.c
+++ libpvl/bbt.c
@@ -19,11 +19,6 @@ struct bbt {
 	unsigned char bits[];
 };
 
-struct bbt_pos {
-	struct bbt *t;
-	size_t pos;
-};
-
 static size_t highest_bit_set(size_t value);
 
 size_t bbt_sizeof(unsigned char order) {
@@ -31,15 +26,11 @@ size_t bbt_sizeof(unsigned char order) {
 		/* return something wild to detect failure
 		 * the init function will also check this
 		 * and fail to initialize */
-		return SIZE_MAX;
+		return 0;
 	}
 	size_t result = sizeof(struct bbt);
 	result += bitset_size(1u << order);
 	return result;
-}
-
-size_t bbt_pos_sizeof() {
-	return sizeof(struct bbt_pos);
 }
 
 struct bbt *bbt_init(unsigned char *at, unsigned char order) {
@@ -57,85 +48,92 @@ struct bbt *bbt_init(unsigned char *at, unsigned char order) {
 	return t;
 }
 
-void bbt_pos_at_depth(struct bbt *t, size_t depth, struct bbt_pos *pos) {
+bbt_pos bbt_left_pos_at_depth(struct bbt *t, size_t depth) {
 	if (depth >= t->order) {
-		return;
+		return 0;
 	}
-	pos->t = t;
-	pos->pos = 1;
-	while (depth && bbt_pos_left_child(pos)) {
+	bbt_pos pos = 1;
+	while (depth && bbt_pos_left_child(t, &pos)) {
 		depth--;
 	}
+	return pos;
 }
 
-_Bool bbt_pos_left_child(struct bbt_pos *pos) {
-	if (bbt_pos_depth(pos) + 1 == pos->t->order) {
+_Bool bbt_pos_left_child(struct bbt *t, bbt_pos *pos) {
+	if (bbt_pos_depth(t, pos) + 1 == t->order) {
 		return 0;
 	}
-	pos->pos = 2 * pos->pos;
+	*pos = 2 * *pos;
 	return 1;
 }
 
-_Bool bbt_pos_right_child(struct bbt_pos *pos) {
-	if (bbt_pos_depth(pos) + 1 == pos->t->order) {
+_Bool bbt_pos_right_child(struct bbt *t, bbt_pos *pos) {
+	if (bbt_pos_depth(t, pos) + 1 == t->order) {
 		return 0;
 	}
-	pos->pos = (2 * pos->pos) + 1;
+	*pos = (2 * *pos) + 1;
 	return 1;
 }
 
-_Bool bbt_pos_left_adjacent(struct bbt_pos *pos) {
-	if (pos->pos <= 1) {
+_Bool bbt_pos_left_adjacent(struct bbt *t, bbt_pos *pos) {
+	(void)(t); /* to keep a unified and future-proof api */
+	if (*pos == 0) {
+		return 0; /* invalid pod */
+	}
+	if (*pos == 1) {
+		return 0; /* root node has no adjacent nodes */
+	}
+	if (highest_bit_set(*pos) != highest_bit_set(*pos - 1)) {
 		return 0;
 	}
-	if (highest_bit_set(pos->pos) != highest_bit_set(pos->pos - 1)) {
-		return 0;
-	}
-	pos->pos -= 1;
+	*pos -= 1;
 	return 1;
 }
 
-_Bool bbt_pos_right_adjacent(struct bbt_pos *pos) {
-	if (pos->pos <= 1) {
+_Bool bbt_pos_right_adjacent(struct bbt *t, bbt_pos *pos) {
+	(void)(t); /* to keep a unified and future-proof api */
+	if (*pos == 0) {
+		return 0; /* invalid pod */
+	}
+	if (*pos == 1) {
+		return 0; /* root node has no adjacent nodes */
+	}
+	if (highest_bit_set(*pos) != highest_bit_set(*pos + 1)) {
 		return 0;
 	}
-	if (pos->pos == pos->t->elements_count - 1) {
-		return 0;
-	}
-	if (highest_bit_set(pos->pos) != highest_bit_set(pos->pos + 1)) {
-		return 0;
-	}
-	pos->pos += 1;
+	*pos += 1;
 	return 1;
 }
 
-_Bool bbt_pos_parent(struct bbt_pos *pos) {
-	size_t parent = pos->pos / 2;
-	if ((parent != pos->pos) && parent != 0) {
-		pos->pos = parent;
+_Bool bbt_pos_parent(struct bbt *t, bbt_pos *pos) {
+	(void)(t); /* to keep a unified and future-proof api */
+	size_t parent = *pos / 2;
+	if ((parent != *pos) && parent != 0) {
+		*pos = parent;
 		return 1;
 	}
 	return 0;
 }
 
-void bbt_pos_set(struct bbt_pos *pos) {
-	bitset_set(pos->t->bits, pos->pos);
+void bbt_pos_set(struct bbt *t, bbt_pos *pos) {
+	bitset_set(t->bits, *pos);
 }
 
-void bbt_pos_clear(struct bbt_pos *pos) {
-	bitset_clear(pos->t->bits, pos->pos);
+void bbt_pos_clear(struct bbt *t, bbt_pos *pos) {
+	bitset_clear(t->bits, *pos);
 }
 
-void bbt_pos_flip(struct bbt_pos *pos) {
-	bitset_flip(pos->t->bits, pos->pos);
+void bbt_pos_flip(struct bbt *t, bbt_pos *pos) {
+	bitset_flip(t->bits, *pos);
 }
 
-_Bool bbt_pos_test(struct bbt_pos *pos) {
-	return bitset_test(pos->t->bits, pos->pos);
+_Bool bbt_pos_test(struct bbt *t, bbt_pos *pos) {
+	return bitset_test(t->bits, *pos);
 }
 
-size_t bbt_pos_depth(struct bbt_pos *pos) {
-	return highest_bit_set(pos->pos);
+size_t bbt_pos_depth(struct bbt *t, bbt_pos *pos) {
+	(void)(t); /* to keep a unified and future-proof api */
+	return highest_bit_set(*pos);
 }
 
 static size_t highest_bit_set(size_t value) {
--- libpvl/bbt.h
+++ libpvl/bbt.h
@@ -9,46 +9,43 @@
 
 struct bbt;
 
-struct bbt_pos;
+typedef size_t bbt_pos;
 
 /* Returns the sizeof of a bbt of the desired order. Keep order small. */
 size_t bbt_sizeof(unsigned char order);
 
-/* Returns the sizeof of the position handle */
-size_t bbt_pos_sizeof();
-
 /* Initializes a boolean binary tree at the specified location. Keep order small. */
 struct bbt *bbt_init(unsigned char *at, unsigned char order);
 
-/* Sets a position at the specified depth */
-void bbt_pos_at_depth(struct bbt *t, size_t depth, struct bbt_pos *pos);
+/* Returns a leftward position at the specified depth */
+bbt_pos bbt_left_pos_at_depth(struct bbt *t, size_t depth);
 
 /* Navigates to the left child node. Returns false if this is a leaf node. */
-_Bool bbt_pos_left_child(struct bbt_pos *pos);
+_Bool bbt_pos_left_child(struct bbt *t, bbt_pos *pos);
 
 /* Navigates to the right child node. Returns false if this is a leaf node. */
-_Bool bbt_pos_right_child(struct bbt_pos *pos);
+_Bool bbt_pos_right_child(struct bbt *t, bbt_pos *pos);
 
 /* Navigates to the left adjacent node. Returns false if there is no left adjacent node. */
-_Bool bbt_pos_left_adjacent(struct bbt_pos *pos);
+_Bool bbt_pos_left_adjacent(struct bbt *t, bbt_pos *pos);
 
 /* Navigates to the right adjacent node. Returns false if there is no right adjacent node. */
-_Bool bbt_pos_right_adjacent(struct bbt_pos *pos);
+_Bool bbt_pos_right_adjacent(struct bbt *t, bbt_pos *pos);
 
 /* Navigates to the patern node. Returns false if this is the root node. */
-_Bool bbt_pos_parent(struct bbt_pos *pos);
+_Bool bbt_pos_parent(struct bbt *t, bbt_pos *pos);
 
 /* Set the value at the indicated position */
-void bbt_pos_set(struct bbt_pos *pos);
+void bbt_pos_set(struct bbt *t, bbt_pos *pos);
 
 /* Clear the value at the indicated  position */
-void bbt_pos_clear(struct bbt_pos *pos);
+void bbt_pos_clear(struct bbt *t, bbt_pos *pos);
 
 /* Flips the value at the indicated  position */
-void bbt_pos_flip(struct bbt_pos *pos);
+void bbt_pos_flip(struct bbt *t, bbt_pos *pos);
 
 /* Returns the value at the indicated  position */
-_Bool bbt_pos_test(struct bbt_pos *pos);
+_Bool bbt_pos_test(struct bbt *t, bbt_pos *pos);
 
 /* Returns the depth at the indicated  position */
-size_t bbt_pos_depth(struct bbt_pos *pos);
+size_t bbt_pos_depth(struct bbt *t, bbt_pos *pos);
